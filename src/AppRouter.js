1ced98803d63aa0ddb265566ae52449509b32388
Michael G <159963384+MichaelGiggey@users.noreply.github.com>
2024-08-29 20:34:23 -0600

    Update AppRouter.js
    Changed Router to HashRouter to prevent 404 error on page reload when not on  home page.

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import Home from './components/home/Home';
 import About from './components/about/About';
@@ -48,11 +48,11 @@ const PageWithTransition = ({ children }) => {
 
 const MainRouter = () => {
   return (
-    <Router>
+    <HashRouter>
       <Navbar />
       <AppRouter />
       <Footer />
-    </Router>
+    </HashRouter>
   );
 };
 
